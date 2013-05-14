2287a52ee5a3d584cf26e87a50f9cd48e2d3f017
Dominic Modalek <dominic@modalek.ch>
2013-05-14 13:26:59 +0200

    add $.unique

---
+++ unique.js
@@ -0,0 +1,36 @@
+
+/*
+ * $.unique
+ */
+
+// Enhanced $.unique function that also handles duplicate values, not just DOM nodes
+
+(function($){
+
+
+    var _old = $.unique;
+
+
+    $.unique = function(arr){
+
+
+        // do the default behavior only if we got an array of elements
+        if (!!arr[0].nodeType){
+            return _old.apply(this,arguments);
+        } else {
+            // reduce the array to contain no dupes via grep/inArray
+            return $.grep(arr,function(v,k){
+                return $.inArray(v,arr) === k;
+            });
+        }
+    };
+})(jQuery);
+
+
+// in use..
+var arr = ['first',7,true,2,7,true,'last','last'];
+$.unique(arr); // ["first", 7, true, 2, "last"]
+
+
+var arr = [1,2,3,4,5,4,3,2,1];
+$.unique(arr); // [1, 2, 3, 4, 5]
